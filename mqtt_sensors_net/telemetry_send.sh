9d89028131f8ab27102fac2a82ffc874d485da56
pico-ivang <russianguru@mail.ru>
2020-12-04 01:37:01 +0300

    divided telemetry and vsens sending

--- mqtt_sensors_net/telemetry_send.sh
+++ mqtt_sensors_net/telemetry_send.sh
@@ -5,12 +5,8 @@ mqtt_port=1357
 mqtt_user=gonya
 mqtt_pass=GonyaUberHaka90
 mqtt_topic=vMon-RA1
+mqtt_request=$1
 
-# send self telemetry
-#/srv/telemetry.sh $mqtt_serv $mqtt_port $mqtt_user $mqtt_pass $mqtt_topic
-bash telemetry.sh $mqtt_serv $mqtt_port $mqtt_user $mqtt_pass $mqtt_topic
-# send sensors data
-#/srv/vsens.sh $mqtt_serv $mqtt_port $mqtt_user $mqtt_pass $mqtt_topic
-bash vsens.sh $mqtt_serv $mqtt_port $mqtt_user $mqtt_pass $mqtt_topic
+bash $mqtt_request $mqtt_serv $mqtt_port $mqtt_user $mqtt_pass $mqtt_topic
 
 exit
