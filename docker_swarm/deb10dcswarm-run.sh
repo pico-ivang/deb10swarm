cb41066ffd1cbca0b64ef437a1e5169bb6ff812c
root <root@wdb.polygon7>
2020-12-24 03:16:05 +0300

    tidy up + crontab variant for dcnodes

---
+++ docker_swarm/deb10dcswarm-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/home/kvm/docker/deb10dcswarm.sh add 20
+/home/kvm/docker/deb10dcswarm.sh telemetry
+/home/kvm/docker/deb10dcswarm.sh vsens
+/home/kvm/docker/deb10dcswarm.sh killall
+
+exit
--- docker_swarm/deb10dcswarm.sh
+++ docker_swarm/deb10dcswarm.sh
@@ -6,7 +6,11 @@
 
 case $1 in
     add)
-	read -p "how many nodes to spawn -> " new_nodes
+	if [ -z $2 ]; then
+	    read -p "how many nodes to spawn -> " new_nodes
+	else
+	    new_nodes=$2
+	 fi
 
 	new_nodes_index=`expr $nodes_count + $new_nodes`
 	new_nodes_start=`expr $nodes_count + 1`
@@ -14,26 +18,21 @@ case $1 in
 	echo "сделаю ноды номерами от "$new_nodes_start" до "$new_nodes_index
 
 	for i in $(seq $new_nodes_start $new_nodes_index); do
-	    docker run -it -d --name deb10dcnode$i --hostname deb10dcnode$i -v /home/kvm/docker/tools:/srv deb10dcnode
+	    docker run -it -d --rm --name deb10dcnode$i --hostname deb10dcnode$i -v /home/kvm/docker/tools:/srv deb10dcnode
 	done
     ;;
     telemetry)
-#	node_count=$(docker ps | grep deb10dcnode | wc -l )
-	for i in $(seq 1 $nodes_count); do
-	    docker exec -it deb10dcnode$i /srv/telemetry.sh
+	for j in $(docker ps -a | grep deb10dcnode | cut -d " " -f1); do
+	    docker exec -it $j /srv/telemetry.sh
 	done
     ;;
     vsens)
-#	node_count=$(docker ps | grep deb10dcnode | wc -l )
-	for i in $(seq 1 $nodes_count); do
-	    docker exec -it deb10dcnode$i /srv/vsens.sh
+	for j in $(docker ps -a | grep deb10dcnode | cut -d " " -f1); do
+	    docker exec -it $j /srv/vsens.sh
 	done
     ;;
     killall)
-	for i in $(seq 1 $nodes_count); do
-	    docker stop deb10dcnode$i
-	    docker rm deb10dcnode$i
-	done
+	docker stop $(docker ps -a | grep deb10dcnode | cut -d " " -f1)
     ;;
     *)
 	echo "usage deb1dcswarm.sh add | telemetry | vsens | killall"
