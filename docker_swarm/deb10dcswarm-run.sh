c56f81248751f394de68504306708a06d2ae6d99
pico-ivang <russianguru@mail.ru>
2020-12-28 15:10:27 +0300

    tidy

--- docker_swarm/deb10dcswarm-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/home/kvm/docker/deb10dcswarm.sh add 20
-/home/kvm/docker/deb10dcswarm.sh telemetry
-/home/kvm/docker/deb10dcswarm.sh vsens
-/home/kvm/docker/deb10dcswarm.sh killall
-
-exit
